b77562ab6c254c222cb7afeebbca9f5664dcccb5
George701 <chernikov.georgii@gmail.com>
2019-09-11 20:29:16 +0300

    Set up client react app and creating landing page and navbar

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Navbar from './layout/Navbar';
+import Landing from './layout/Landing';
+
+const App = () => {
+    return (
+        <Fragment>
+            <Navbar />
+            <Landing />
+        </Fragment>
+    )
+}
+
+export default App;
